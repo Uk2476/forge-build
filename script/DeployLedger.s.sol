ee0a9da23dc03e13385d3acd5f972524add6a31f
Uk2476 <kuttkarsh028@gmail.com>
2026-01-09 19:47:01 +0000

    wrote a script to deploy the contract

--- script/DeployLedger.s.sol
+++ script/DeployLedger.s.sol
@@ -1,4 +1,13 @@
 //SPDX_License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract DeployLedger {}
+import {Script, console} from "forge-std/Script.sol";
+import {Ledger} from "../src/Ledger.sol";
+
+contract DeployLedger is Script {
+    function run() public {
+        vm.startBroadcast();
+        Ledger ledger = new Ledger();
+        vm.stopBroadcast();
+    }
+}
